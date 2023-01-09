94b0174265bab0ef83409b96fbc236e2b2970785
zuedev <zuedev@gmail.com>
2023-01-09 08:25:59 +0000

    fix data inheritance for rifleman loadout

--- src/data/loadouts/uag/rifleman.js
+++ src/data/loadouts/uag/rifleman.js
@@ -1,9 +1,9 @@
 import unarmed from "./unarmed.js";
 
 export default {
-  name: "Rifleman",
-  description: "Rifleman",
   ...unarmed,
+  name: "Rifleman",
+  description: "Standard rifleman loadout.",
   rifle: "UAG_MSBS65v2",
   rifleMags: [["30Rnd_65x39_caseless_msbs_mag", 10]],
   handgun: "CUP_hgun_Glock17_blk",
