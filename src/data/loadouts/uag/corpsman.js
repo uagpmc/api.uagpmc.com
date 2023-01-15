fffdf2f0bea880cf560e01bec1183d1a725f396b
zuedev <zuedev@gmail.com>
2023-01-15 00:28:14 +0000

    give corpsman a lighter vest

--- src/data/loadouts/uag/corpsman.js
+++ src/data/loadouts/uag/corpsman.js
@@ -1,9 +1,14 @@
 import rifleman from "./rifleman.js";
-import { MEDICAL_SUPPLIES_CORPSMAN } from "./_standard_issue.js";
+import {
+  HEADGEAR_LIGHT,
+  VEST_LIGHT,
+  MEDICAL_SUPPLIES_CORPSMAN,
+} from "./_standard_issue.js";
 
 export default {
   ...rifleman,
   name: "Corpsman",
   description: "Standard corpsman loadout.",
+  vest: VEST_LIGHT,
   medicalSupplies: [...MEDICAL_SUPPLIES_CORPSMAN],
 };
