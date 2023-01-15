7a2606fa713d9a380ebbbcf7eb86e82f0c7516b8
zuedev <zuedev@gmail.com>
2023-01-15 00:30:20 +0000

    give marksman a lighter vest

--- src/data/loadouts/uag/marksman.js
+++ src/data/loadouts/uag/marksman.js
@@ -1,10 +1,11 @@
 import rifleman from "./rifleman.js";
-import { DMR, DMR_AMMO, DMR_ITEMS } from "./_standard_issue.js";
+import { VEST_LIGHT, DMR, DMR_AMMO, DMR_ITEMS } from "./_standard_issue.js";
 
 export default {
   ...rifleman,
   name: "Marksman",
   description: "Standard marksman loadout.",
+  vest: VEST_LIGHT,
   rifle: DMR,
   rifleAmmo: [[DMR_AMMO, 10]],
   rifleItems: DMR_ITEMS,
