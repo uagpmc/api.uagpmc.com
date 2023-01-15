70bb8dfc593046add856370b725b23e7c5162df7
zuedev <zuedev@gmail.com>
2023-01-15 00:35:45 +0000

    sniper now uses light helmet and vest

--- src/data/loadouts/uag/sniper.js
+++ src/data/loadouts/uag/sniper.js
@@ -1,10 +1,18 @@
 import rifleman from "./rifleman.js";
-import { SNIPER, SNIPER_AMMO, SNIPER_ITEMS } from "./_standard_issue.js";
+import {
+  HEADGEAR_LIGHT,
+  VEST_LIGHT,
+  SNIPER,
+  SNIPER_AMMO,
+  SNIPER_ITEMS,
+} from "./_standard_issue.js";
 
 export default {
   ...rifleman,
   name: "Sniper",
   description: "Standard sniper loadout.",
+  headgear: HEADGEAR_LIGHT,
+  vest: VEST_LIGHT,
   rifle: SNIPER,
   rifleAmmo: [[SNIPER_AMMO, 10]],
   rifleItems: SNIPER_ITEMS,
