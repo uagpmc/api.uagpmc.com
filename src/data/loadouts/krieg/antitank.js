93f4c197ad9ad371227a1ae297bf776414aaeff5
zuedev <zuedev@gmail.com>
2023-01-22 13:20:46 +0000

    add antitank loadout for krieg

---
+++ src/data/loadouts/krieg/antitank.js
@@ -0,0 +1,10 @@
+import rifleman from "./rifleman.js";
+
+export default {
+  ...rifleman,
+  name: "Krieg Anti-Tank",
+  description: "For taking out enemy vehicles.",
+  backpack: "DKoK_AT_BackPack",
+  launcher: "MissileLauncherDKOKBlack",
+  launcherAmmo: [["MLAT_Mag", 5]],
+};
