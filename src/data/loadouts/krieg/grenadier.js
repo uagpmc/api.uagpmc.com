ad7c9282628c3f76a382755dab5e1241601d31fa
zuedev <zuedev@gmail.com>
2023-01-22 13:13:15 +0000

    add grenadier loadout for krieg

---
+++ src/data/loadouts/krieg/grenadier.js
@@ -0,0 +1,11 @@
+import machinegunner from "./machinegunner.js";
+
+export default {
+  ...machinegunner,
+  name: "Krieg Plasmagunner",
+  description:
+    "A plasmagun is a weapon that fires a bolt of superheated plasma that can melt through armour.",
+  backpack: "DKoK_PlasmaBackPack",
+  rifle: "DK_PlasmaGun",
+  rifleAmmo: [["DK_PlasmaGun_Flask", 10]],
+};
