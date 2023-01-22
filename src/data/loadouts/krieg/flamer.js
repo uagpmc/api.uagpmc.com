354c026f88920797e8f540cbad5fcbc636e218df
zuedev <zuedev@gmail.com>
2023-01-22 13:19:05 +0000

    add flamer loadout for krieg

---
+++ src/data/loadouts/krieg/flamer.js
@@ -0,0 +1,10 @@
+import rifleman from "./rifleman.js";
+
+export default {
+  ...rifleman,
+  name: "Krieg Flamer",
+  description: "SCORCH THE EARTH!",
+  backpack: "M2_Flamethrower_Balloons_Pipe",
+  rifle: "TIOW_Krieg_Flamer",
+  rifleAmmo: [["TIOW_Krieg_Flamer_mag", 10]],
+};
