690f5d1168f65863ac89bce855a75864417e36e6
zuedev <zuedev@gmail.com>
2023-01-22 12:43:54 +0000

    add krieg rifleman loadout

---
+++ src/data/loadouts/krieg/rifleman.js
@@ -0,0 +1,12 @@
+import rifleman from "../uag/rifleman.js";
+
+export default {
+  ...rifleman,
+  name: "Krieg Guardsman",
+  description: "The bulwark of the Krieg army.",
+  //   rifle: RIFLE,
+  //   rifleAmmo: [[RIFLE_AMMO, 10]],
+  //   rifleItems: RIFLE_ITEMS,
+  handgun: null,
+  handgunAmmo: null,
+};
