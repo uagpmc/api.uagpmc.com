c63352cc345d89301c0c3611ccf5f7c04a8b2b52
zuedev <zuedev@gmail.com>
2023-02-05 20:06:44 +0000

    remove handgun from rifleman loadout

--- src/data/loadouts/uag/rifleman.js
+++ src/data/loadouts/uag/rifleman.js
@@ -3,8 +3,6 @@ import {
   RIFLE,
   RIFLE_AMMO,
   RIFLE_ITEMS,
-  HANDGUN,
-  HANDGUN_AMMO,
   FRAG_GRENADE,
   SMOKE_GRENADE,
 } from "./_standard_issue.js";
@@ -15,7 +13,5 @@ export default {
   rifle: RIFLE,
   rifleAmmo: [[RIFLE_AMMO, 10]],
   rifleItems: RIFLE_ITEMS,
-  handgun: HANDGUN,
-  handgunAmmo: [[HANDGUN_AMMO, 2]],
   items: [...unarmed.items, [FRAG_GRENADE, 1], [SMOKE_GRENADE, 1]],
 };
