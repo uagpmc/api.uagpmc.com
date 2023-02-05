4d175753fc7cb4d56cd58a1fadcf5c07236b0de5
zuedev <zuedev@gmail.com>
2023-02-05 18:12:30 +0000

    cont.

--- src/data/loadouts/uag/medic.js
+++ src/data/loadouts/uag/medic.js
@@ -1,7 +1,8 @@
+import rifleman from "./rifleman.js";
 import { BACKPACK_MEDIUM, MEDICAL_SUPPLIES_MEDIC } from "./_standard_issue.js";
 
 export default {
-  ...corpsman,
+  ...rifleman,
   name: "Medic",
   backpack: BACKPACK_MEDIUM,
   medicalSupplies: [...MEDICAL_SUPPLIES_MEDIC],
